18d5a06740addebd9b544a3c769cc77ee2d5552f
Mutton9558 <69904687+Mutton9558@users.noreply.github.com>
2024-09-24 23:53:27 +0800

    Added pipes and pipe spawning function

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour{
+    public float moveSpeed = 1;
+    public float deadZone = -31;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (transform.position.x < deadZone)
+        {
+            // Debug.Log("Message") is a good way to see messages when a function is executed and helps debugging.
+            Destroy(gameObject);
+        }
+    }
+}
