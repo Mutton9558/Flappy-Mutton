5e1f5d2fd681b5079d352cc53f371b7a2c483f06
Mutton9558 <69904687+Mutton9558@users.noreply.github.com>
2024-09-24 22:30:49 +0800

    Jump feature

--- Assets/ObjectScript.cs
+++ Assets/ObjectScript.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectScript : MonoBehaviour
-{
+public class ObjectScript : MonoBehaviour{
+    public Rigidbody2D myRigitBody;
+    public float flapStrength;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space)){
+            myRigitBody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
